b6a9752ff1f9dac39b31587531c5f2d9662f0249
Heaven <154735143+codinghub306@users.noreply.github.com>
2025-06-20 22:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Ensure correct key name
+    if not BOT_TOKEN:
+        raise ValueError("❌ BOT_TOKEN is missing from environment variables.")
+        
     API_ID = int(os.environ.get("API_ID", "20928172"))  # Added key name and default value
     API_HASH = os.environ.get("API_HASH", "48ed56c8db54f85d232f576b150360ef")  # Added key name for consistency
 
