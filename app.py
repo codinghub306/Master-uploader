21ffcca08b0ad183e3382f7744d0b9a165627180
Heaven <154735143+codinghub306@users.noreply.github.com>
2025-06-20 22:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Tech VJ'
 
-
 if __name__ == "__main__":
-    app.run()
-  
+    # For Docker/Heroku deployment
+    app.run(host="0.0.0.0", port=8080)
